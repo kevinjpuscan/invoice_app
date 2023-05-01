522833514a0f6265cae5f91c0a56c6a47d81508c
Kevin Puscán Ortiz <kevinjpuscan@gmail.com>
2023-05-01 14:20:46 +0200

    style(theme) :add default theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,12 @@ export const metadata = {
   title: "Notes Manager",
   description: "Your notes manager",
 };
+import { League_Spartan } from 'next/font/google'
 
+const leagueSpartan = League_Spartan({
+  subsets:['latin'],
+  weight:['500','700']
+});
 export default function RootLayout({
   children,
 }: {
@@ -14,12 +19,13 @@ export default function RootLayout({
     <head>
       <meta charSet="UTF-8"/>
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com"/>
+      <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@300;600&display=swap" rel="stylesheet"></link>
       <link rel="icon" type="image/png" sizes="32x32" href="./assets/icon/favicon-32x32.png"/>
-      
       <title>Frontend Mentor | Invoice app</title>
     </head>
-      <body>{children}</body>
+      <body className={leagueSpartan.className}>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 
 export default function App(): JSX.Element {
   return (
-    <div data-test="example">
-      Hola Mundo
+    <div>
+      <h1>Hola Mundo</h1>
+      <h2>Esto es un subtitulo</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis repudiandae quisquam minus nisi ab est omnis nemo quidem magni ipsa beatae, ratione ipsum! Nisi dicta quas vel deleniti est sequi!</p>
     </div>
   );
 }
