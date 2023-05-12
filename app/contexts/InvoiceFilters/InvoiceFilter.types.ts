dffbaa3c71e5fa6d74d88aa22c3fa0740ec60a1e
Kevin Puscán Ortiz <kevinjpuscan@gmail.com>
2023-05-12 23:48:48 +0200

    refactor(invoices): remove type invoicefilter

--- app/contexts/InvoiceFilters/InvoiceFilter.types.ts
+++
@@ -1,8 +0,0 @@
-import { type CheckBoxListItem } from "@/app/design-system/CheckBoxList/CheckBoxList.types";
-import { type StatusType } from "@/core/domain/Invoice";
-
-export interface InvoiceFilter extends CheckBoxListItem {
-  label: string;
-  value: StatusType;
-  checked: boolean;
-}
